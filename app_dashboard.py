2181eb4eacfe69b14443d9746132fd6a5d9b25f3
valuata <admin@valuata.com.br>
2024-11-28 17:58:10 -0300

    Update app_dashboard.py

--- app_dashboard.py
+++ app_dashboard.py
@@ -95,7 +95,7 @@ from github import Github
 
 
 token = os.getenv('GITHUB_TOKEN')
-repo_name = "valuata/dashboard"  #GitHub repository name
+repo_name = "valuata/Dashboard"  #GitHub repository name
 file_name = "Carga_Consumo_atualizado.csv"  #  desired file name
 commit_message = "Update Carga_Consumo"  #  commit message
 
