2c197df115732528cfbe876c2cd78ceb065a36fe
FernandoMS <124934964+FerMendess@users.noreply.github.com>
2025-04-18 00:48:39 -0300

    Update Carga.py

--- Carga.py
+++ Carga.py
@@ -1,20 +1,20 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objs as go
-from datetime import datetime, timedelta
+from datetime import datetime
 from github import Github
 from babel import Locale
-from babel.numbers import format_decimal, format_currency
+from babel.numbers import format_decimal
 from babel.dates import format_date
+import io
 
+# Configurações iniciais da página Streamlit e CSS
 
 st.set_page_config(page_title="Carga", layout="wide")
 import streamlit as st
 
-# Hide the header action elements (e.g., hamburger menu)
 st.html("<style>[data-testid='stHeaderActionElements'] {display: none;}</style>")
 
-# Custom CSS for the page background and font
 st.markdown("""
     <style>
         * {
@@ -120,19 +120,19 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
+# Configurações dos ícones de interface dos gráficos plotly
 config = {
-    'displaylogo': False,    # Desabilita o logo Plotly na barra
-    'modeBarButtonsToRemove': ['zoomIn2d', 'zoomOut2d', 'lasso2d', 'select2d'],  # Remove botões de zoom
-    'modeBarButtonsToAdd': ['resetScale2d'],  # Adiciona um botão para resetar o gráfico
-    'showTips': False,        # Desabilita dicas de ferramenta
-    'responsive': False      # Desabilita o redimensionamento automático
+    'displaylogo': False,    
+    'modeBarButtonsToRemove': ['zoomIn2d', 'zoomOut2d', 'lasso2d', 'select2d'],  
+    'modeBarButtonsToAdd': ['resetScale2d'], 
+    'showTips': False,       
+    'responsive': False      
 }
 
 
 def aggregate_data(data, frequency):
     data = data.set_index(['id_subsistema', 'din_instante'])
 
-    # Ajustando a frequência para resampling
     if frequency == 'Diário':
         data = data.groupby(level='id_subsistema').resample('D', level='din_instante').last()
     elif frequency == 'Semanal':
@@ -145,8 +145,7 @@ def aggregate_data(data, frequency):
     return data[['din_instante', 'id_subsistema', 'val_cargaenergiamwmed']]
 
 def format_week_date(date):
-    # Calcula o número da semana dentro do mês
-    week_number = (date.day - 1) // 7 + 1  # Semanas de 7 dias
+    week_number = (date.day - 1) // 7 + 1  
     return f"S{week_number}/{format_date(date, format='MM/yyyy', locale='pt_BR').upper()}"
 def format_month_date(date):
     return format_date(date, format='MM/yyyy', locale='pt_BR').upper()
@@ -170,6 +169,7 @@ arquivo_data = 'data_atual.txt'
 data_arquivo = ler_data_arquivo()
 locale = Locale('pt', 'BR')
 
+# Atualização automática dos dados
 if (data_atual.date() > data_arquivo.date() and data_atual.hour >= 2):
     def authenticate_github(token):
         g = Github(token)
@@ -182,7 +182,6 @@ if (data_atual.date() > data_arquivo.date() and data_atual.hour >= 2):
 
         file = repo.get_contents(file_name)
 
-            # Update the file
         repo.update_file(file.path, commit_message, file_content, file.sha)
 
     def atualizar_data_arquivo():
@@ -200,9 +199,9 @@ if (data_atual.date() > data_arquivo.date() and data_atual.hour >= 2):
             token2 = file.read()
 
     token = token1 + token2
-    repo_name = "valuata/Dashboard"  #GitHub repository name
-    file_name = "Carga_Consumo_atualizado.csv"  #  desired file name
-    commit_message = "Update Carga_Consumo"  #  commit message
+    repo_name = "valuata/Dashboard"  
+    file_name = "Carga_Consumo_atualizado.csv" 
+    commit_message = "Update Carga_Consumo"  
 
     failure = False
     i = 2000
@@ -211,10 +210,8 @@ if (data_atual.date() > data_arquivo.date() and data_atual.hour >= 2):
     while failure == False:
         url = f'https://ons-aws-prod-opendata.s3.amazonaws.com/dataset/carga_energia_di/CARGA_ENERGIA_{i}.csv'
         try:
-            # Lendo o CSV diretamente da URL com delimitador ';'
             dados_carga = pd.read_csv(url, delimiter=';')
         except Exception as e:
-            # Caso haja erro ao carregar o arquivo, sai do loop
             failure = True
         if i == 2000:
             df_carga = dados_carga
@@ -227,28 +224,26 @@ if (data_atual.date() > data_arquivo.date() and data_atual.hour >= 2):
     df_carga.replace({'SE': 'SE/CO'}, inplace=True)
     carga_data = df_carga
     
-    # Atualizar o arquivo .txt com a data atual
     atualizar_data_arquivo()
     
     file_content = carga_data.to_csv(index=False)
     push_to_github(repo_name, file_name, commit_message, file_content, token)
 
+# Carregar os dados do github
 carga_data = pd.read_csv('Carga_Consumo_atualizado.csv')
-# Carregar os dados
 
 st.title("Carga")
 
-# Garantir que a coluna 'din_instante' seja convertida corretamente para datetime
+# Gorrigir as datas para datetime
 carga_data['din_instante'] = pd.to_datetime(carga_data['din_instante'].str.slice(0, 10), format="%Y-%m-%d")
 
-# Controlador de intervalo de datas
+
 min_date = carga_data['din_instante'].min().date()
 max_date = carga_data['din_instante'].max().date()
 
 # Calcular o intervalo de 5 anos atrás
 start_date_default = max_date.replace(year=max_date.year - 5, month=1, day=1)
 
-# Selecione o intervalo de datas usando um slider
 if 'slider_dates' not in st.session_state:
     st.session_state.slider_dates = (start_date_default, max_date)
 
@@ -260,39 +255,32 @@ start_date_slider, end_date_slider = st.slider(
     format="DD/MM/YYYY"
 )
 
-# Atualizar os valores dos date inputs conforme o slider
 st.session_state.slider_dates = (start_date_slider, end_date_slider)
 
-# Colunas de layout para os controles
 col3, col4, col1, col2 = st.columns([1, 1, 1, 2])
 with col1:
-    frequency = st.radio("**Frequência**", ['Diário', 'Semanal', 'Mensal'], index=2)  # Start with 'Mensal'
+    frequency = st.radio("**Frequência**", ['Diário', 'Semanal', 'Mensal'], index=2)  
 with col2:
     selected_subsystems = st.multiselect(
         "**Selecione os submercados**",
         options=['SE/CO', 'S', 'NE', 'N'],
-        default=['SE/CO', 'S', 'NE', 'N'], placeholder='Escolha uma opção'  # Seleção padrão
+        default=['SE/CO', 'S', 'NE', 'N'], placeholder='Escolha uma opção'  
     )
 with col3:
-    # Atualiza o valor do date input para o valor do slider
     start_date_input = st.date_input("**Início**", min_value=min_date, max_value=max_date, value=start_date_slider, format="DD/MM/YYYY")
     if start_date_input != start_date_slider:
-        st.session_state.slider_dates = (start_date_input, end_date_slider)  # Atualiza o slider com a nova data
-        st.rerun()  # Força a atualização imediata
+        st.session_state.slider_dates = (start_date_input, end_date_slider) 
+        st.rerun()  
 with col4:
-    # Atualiza o valor do date input para o valor do slider
     end_date_input = st.date_input("**Fim**", min_value=min_date, max_value=max_date, value=end_date_slider, format="DD/MM/YYYY")
     if end_date_input != end_date_slider:
-        st.session_state.slider_dates = (start_date_slider, end_date_input)  # Atualiza o slider com a nova data
-        st.rerun()  # Força a atualização imediata
+        st.session_state.slider_dates = (start_date_slider, end_date_input)  
+        st.rerun()  
 
-# Filtragem dos dados com base nas datas selecionadas
 filtered_data = carga_data[(carga_data['din_instante'] >= pd.to_datetime(start_date_input)) & 
                            (carga_data['din_instante'] <= pd.to_datetime(end_date_input))]
-# Agregar os dados com base na frequência selecionada
 agg_data = aggregate_data(filtered_data, frequency)
 
-# Filtrar os dados para incluir apenas os subsistemas selecionados
 agg_data = agg_data[agg_data['id_subsistema'].isin(selected_subsystems)]
 with st.spinner('Carregando gráfico...'):
 
@@ -402,22 +390,18 @@ with st.spinner('Carregando gráfico...'):
 
 
 
-        # 1. Extrair os anos dos dados
         agg_data['year'] = agg_data['din_instante'].dt.year
 
-        # 2. Encontrar a primeira ocorrência de cada ano
         first_occurrences = agg_data.groupby('year')['din_instante'].min()
         first_occurrences = first_occurrences.to_frame()
 
-        # 4. Formatar as datas de ticks (ajustar conforme necessário)
         formatted_ticks = [format_month_date_tick(date) for date in first_occurrences['din_instante']]
 
-        # 5. Atualizar o eixo X com essas datas e os rótulos formatados
         fig.update_xaxes(
             tickmode='array',
-            tickvals=first_occurrences['din_instante'],  # Posições no eixo X para as primeiras datas de cada ano
-            ticktext=formatted_ticks,  # Rótulos formatados para os ticks
-            tickangle=0  # Ajuste do ângulo dos rótulos, se necessário
+            tickvals=first_occurrences['din_instante'],  
+            ticktext=formatted_ticks,  
+            tickangle=0  
         )
 
 
@@ -439,15 +423,14 @@ agg_data = agg_data.drop(columns=['week_label', 'year'])
 
 agg_data = agg_data.rename(columns={'din_instante': 'Data', 'id_subsistema': 'Submercado', 'val_cargaenergiamwmed': 'Carga (MWmed)'})
 
-import io
 excel_file = io.BytesIO()
 with pd.ExcelWriter(excel_file, engine='xlsxwriter') as writer:
     agg_data.to_excel(writer, index=False, sheet_name='Sheet1')
 
-# Fazendo o download do arquivo Excel
+# Fazendo o download do gráfico para Excel
 st.download_button(
     label="DOWNLOAD",
     data=excel_file.getvalue(),
-    file_name=f'Dados_Carga_({data_atual}).xlsx',  # Certifique-se de definir a variável data_atual
+    file_name=f'Dados_Carga_({data_atual}).xlsx', 
     mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 )
